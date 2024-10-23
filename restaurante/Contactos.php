e69a41ad86ba94d8b083a96cae6cc14fb877483a
Jazmin Castro <146500089+JazminCastroo@users.noreply.github.com>
2024-10-22 22:13:32 -0300

    Add files via upload

--- restaurante/Contactos.php
+++ restaurante/Contactos.php
@@ -1,23 +1,26 @@
-<?php
-session_start();
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/contacto.css">
     <title>Document</title>
 </head>
 <body>
-    <header>
+<header>
         <h2>RESTAURANTE</h2>
         <div class="opciones">
             <li><a href="">Ver Menú</a></li>
             <li><a href="">Tus Pedidos</a></li>
             <li><a href="">Puntos de Encuentro</a></li>
-            <li><a href="Contactos.php">Contactos</a></li>
+            <li><a href="">Contactos</a></li>
         </div>
     </header>
+    
+    <div class="contacto">
+    <p> Mail: restauranteelmejor@mail.com</p>
+    <p>Telefono: 1130789651</p>
+    </div>
+    
 </body>
 </html>
--- restaurante/php/register.php
+++ restaurante/php/register.php
@@ -6,7 +6,7 @@ include('conn.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="../css/login.css"> 
     <title>Document</title>
 </head>
 <body>
