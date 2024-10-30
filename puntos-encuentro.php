dc6fb9f5559cb1563cb6587d1737e35d66eec3bd
Jazmin Castro <146500089+JazminCastroo@users.noreply.github.com>
2024-10-29 22:01:04 -0300

    Add files via upload

---
+++ puntos-encuentro.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/scrollbar.css">
+    <link rel="stylesheet" href="css/puntos-encuentr.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=dinner_dining" />
+    <title>Document</title>
+</head>
+<body>
+<header>
+        <h2><span class="material-symbols-outlined">dinner_dining</span>RESTAURANTE</h2>
+        <div class="opciones">
+            <li><a href="index.php">Ver Menú</a></li>
+            <li><a href="carrito.php">Tus Pedidos</a></li>
+            <li><a href="puntos-encuentro.php">Puntos de Encuentro</a></li>
+            <li><a href="Contactos.php">Contactos</a></li>
+        </div>
+    </header>
+    <div class="encuentros">
+    <p>Podes encontrarnos aqui :)</p> <br>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d9263.841378363524!2d-58.361707612441975!3d-34.66125715775408!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2sar!4v1730242420906!5m2!1ses!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+</body>
+</html>
